20b93af897661a29ec73c498784da4920e39c2e8
Demna <kapanadze1987@gmail.com>
2023-05-23 18:16:57 +0400

    first commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,31 @@
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
+
+export function buldLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) =>
+                            Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path].[name]__[local]'
+                            : '[hash:base64:8]',
+                    },
+                },
+            },
+            'sass-loader',
+        ],
+    };
+    const typescriptloader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    };
+    return [typescriptloader, cssLoader];
+}
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export function someFn(arg: number): string {
+    console.log('RANDOM FUNCTION demnatoreliplplp49496431454647460132101');
+    return '';
+}
